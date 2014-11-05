7d3ad458852b1388619b6fea06293fb0e666f978
Robert Schardt <robert.schardt@gmx.de>
2014-11-05 20:43:51 +0100

    + XML-Template

---
+++ Client/Javaapplication/src/de/oszimt/DreiSchichten/database/XMLAccess.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.oszimt.DreiSchichten.database;
+
+/**
+ *
+ * @author Robert Schardt
+ */
+
+/*public enum XMLTable {
+    XMLTable_Beruf,
+    XMLTable_Berufstyp,
+    XMLTable_Dorf,
+    XMLTable_Lager,
+    XMLTable_LagerBestand,
+    XMLTable_Mitglied,
+    XMLTable_Ressource
+}*/
+
+public class XMLAccess {
+    
+    // oder wie bei DB schon für jede Tabelle eine eigene Methode -.-, ne das ist bescheuert
+    
+    // müssen über ihren Tabellentyp und primaryIndex identifiziert werden
+    public void SetXMLEntry()
+    {
+    
+    }
+    
+    public void GetXMLEntry()
+    {
+    
+    }
+    
+    public void RemXMLEntry()
+    {
+    
+    }
+    
+    public void AddXMLEntry()
+    {
+    
+    }
+}
