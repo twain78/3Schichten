a3e508e1a7989e9d086f8d74614d9c5f1dc2dc9a
Robert Schardt <robert.schardt@gmx.de>
2014-12-07 21:48:23 +0100

    View-Interface mit Factory

---
+++ Client/Javaapplication/src/de/oszimt/DreiSchichten/controller/ViewFactory.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.oszimt.DreiSchichten.controller;
+
+/**
+ *
+ * @author keinMensch
+ */
+public class ViewFactory {
+    
+}
